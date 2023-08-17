b69d14b83b6269b2b378b56d8c05fe7ee2f3f82a
Hyunsu17 <62607957+Hyunsu17@users.noreply.github.com>
2023-08-17 21:01:45 +0900

    0815

--- src/main/java/com/cos/blog/controller/UserController.java
+++ src/main/java/com/cos/blog/controller/UserController.java
@@ -84,7 +84,7 @@ public class UserController {
 
     @PostMapping("/api/exercise")
     public ResponseEntity<Object> saveExercises(@RequestBody JsonNode saveObj){
-
+        return null;
     }
 
 }
